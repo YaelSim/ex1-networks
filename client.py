310517d5a14e8e90d31160a8b8cb366b81632ceb
YaelSim <58948260+YaelSim@users.noreply.github.com>
2020-11-11 16:19:27 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -1,13 +1,15 @@
 import sys
+import socket
 
 def main():
     server_ip = sys.argv[1]
     server_port = sys.argv[2]
-
-
-
-
-
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+        massage = input().encode()
+        server_socket.sendto(massage, (server_ip, server_port))  # send massage to server
+        data, addr = server_socket.recvfrom(1024)  # receive answer from server
+        print(data.decode())  # print the received massage
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,24 +1,65 @@
 import sys
 import socket
+from datetime import datetime
+
+
 
 def main():
     port = sys.argv[1]
-    ip_address = sys.argv[2]
+    ip_parent_address = sys.argv[2]
     parent_port = sys.argv[3]
     ips_file = sys.argv[4]
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP server.
+    parent_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # connecting to parent server
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # connecting to client (to me)
+    client_socket.bind(('', int(port)))
+
     ips_list = []
     file = open(ips_file)
     for lines in file:
         lines = lines.strip("\n")
-        ips_list.append(lines.split(","))
-    n=1
+        lines = lines.split(",")
+        # the time - alive remaining from the ttl.
+        lines.append(datetime(1, 1, 1, 1, 1, 1))  # initial time.
+        ips_list.append(lines)
+
+    ips_dict = {x[0]: x[1:] for x in ips_list}  # convert the list of lists to dictionary
+
+    """
+    data = "mail.biu.ac.il"
+    if data in ips_dict:
+        desired_ip = ips_dict[data][0]
+        desired_ttl = ips_dict[data][1]
+        ips_dict[data][2] = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        i = is_this_entry_relevant(desired_ttl, ips_dict[data][2])
+        n=1
+    """
+
+    while True:
+        data, addr = client_socket.recvfrom(1024)
+        data = data.decode()  # check if needed.
+        # is_in_list = any(data in sublist for sublist in ips_list)
+        # if true -> return its ip address. o.w- ask the parent for it
+        # todo check what to do with TTL later. **************************
+
+        if data in ips_dict:
+            desired_ip = ips_dict[data][0]
+            desired_ttl = ips_dict[data][1]
+            ips_dict[data][2] = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+            # if the ttl not finished then we can sent the entry to the client, o.w we need to ask parent
+            if  is_this_entry_relevant(desired_ttl, ips_dict[data][2]):
+                # check if encode is neede*****
+                client_socket.sendto(desired_ip.encode,addr)
+
+
+        else:
+            r = 1
 
 
 
-
-
-
+def is_this_entry_relevant(ttl, remaining_time):
+    remaining_time = datetime.strptime(remaining_time, '%Y-%m-%d %H:%M:%S.%f')  # convert str to datetime.
+    result = (datetime.now() - remaining_time).total_seconds()
+    return result < float(ttl)
 
 if __name__ == "__main__":
     main()
